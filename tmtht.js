ae5e60969aa643388065d689829257b20563ee18
rickdog <rickmp3s@gmail.com>
2015-01-25 17:22:42 -0600

    Update tmtht.js

--- tmtht.js
+++ tmtht.js
@@ -2,31 +2,29 @@
  * (e.g. http://www.themillertellshertale.co.uk/index.cfm?page=index&show=240),
  * and output it as an unordered list as a prompt.
  */
- alert(1);
-(function(){
-  alert(2);
-  
-var out = document.getElementsByName("playlistform")[0].childNodes[0].nodeValue;
-var re = new RegExp('\\n', 'g');
-out = out.replace(re, '') + '<br><p>For more information about all the music played and links to the artist websites, please check <a href="http://www.themillertellshertale.co.uk" rel="nofollow">themillertellshertale.co.uk</a></p><ul>';
 
-var rows = $('.website').tBodies[0].rows.length;
-for (i = 1; i < rows; i++)
+function parse_table()
 {
-  var row = $('.website').tBodies[0].rows[i];
-  var cols = row.cells;
-  if (cols.length == 1)
-    continue;
-  out += "<li>";
-  out += cols[0].innerHTML + ": ";  // artist
-  out += cols[2].innerHTML + " (";  // song
-  out += cols[1].innerHTML + ", ";  // album
-  out += cols[3].innerHTML + ', ';  // label
-  out += cols[4].innerHTML + ")</li>";
+  var out = document.getElementsByName("playlistform")[0].childNodes[0].nodeValue;
+  var re = new RegExp('\\n', 'g');
+  out = out.replace(re, '') + '<br><p>For more information about all the music played and links to the artist websites, please check <a href="http://www.themillertellshertale.co.uk" rel="nofollow">themillertellshertale.co.uk</a></p><ul>';
+  
+  var rows = $('.website').tBodies[0].rows.length;
+  for (i = 1; i < rows; i++)
+  {
+    var row = $('.website').tBodies[0].rows[i];
+    var cols = row.cells;
+    if (cols.length == 1)
+      continue;
+    out += "<li>";
+    out += cols[0].innerHTML + ": ";  // artist
+    out += cols[2].innerHTML + " (";  // song
+    out += cols[1].innerHTML + ", ";  // album
+    out += cols[3].innerHTML + ', ';  // label
+    out += cols[4].innerHTML + ")</li>";
+  }
+  
+  out += "</ul>";
+  console.log(out);
+  prompt("Converted", out);
 }
-
-out += "</ul>";
-console.log(out);
-prompt("Converted", out);
-
-})();
